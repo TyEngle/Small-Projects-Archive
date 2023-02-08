c3cc05d40bb0befd49bfe3f201cb370e4272dbf6
TyEngle <95366088+TyEngle@users.noreply.github.com>
2023-02-08 10:52:09 -0330

    Split Landing Add
    Uploaded Split-Landing program via Github

---
+++ Split-Landing/script.js
@@ -0,0 +1,22 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-right")
+);
+
+// great function to help scale values up or down
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
